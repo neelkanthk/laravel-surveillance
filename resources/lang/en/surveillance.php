7f1344100660f86c8f004cb375340c3007a2fa68
Viktor Szépe <viktor@szepe.net>
2020-11-21 23:24:45 +0100

    Remove duplicate key from language array
    A PHP array cannot have two elements with the same key.
    
    Please consider running static analysis.

--- resources/lang/en/surveillance.php
+++ resources/lang/en/surveillance.php
@@ -3,7 +3,6 @@
 return [
     "messages" => [
         'enabled' => "\nSurveillance has been enabled.\n",
-        'already-enabled' => "\nSurveillance is already enabled.\n",
         'disabled' => "\nSurveillance has been disabled.\n",
         'already-enabled' => "\nSurveillance is already disabled.\n",
         'blocked' => "\nAccess has been blocked.\n",
@@ -11,9 +10,9 @@ return [
         'unblocked' => "\nAccess has been unblocked.\n",
         'already-unblocked' => "\nAccess is already unblocked.\n",
         'record-removed' => "\nSurveillance record(s) removed.\n",
-        "record-not-found" => "\nNo existing record(s) found for type: [:type] and value: [:value].\n",
-        "confirm" => "Are you sure?",
-        "console-validation-failed" => "\nValidation failed.\n",
-        "console-allowed-types" => "Invalid 'type'. Only following types are allowed: [ :types ]"
+        'record-not-found' => "\nNo existing record(s) found for type: [:type] and value: [:value].\n",
+        'confirm' => "Are you sure?",
+        'console-validation-failed' => "\nValidation failed.\n",
+        'console-allowed-types' => "Invalid 'type'. Only following types are allowed: [ :types ]"
     ]
 ];
